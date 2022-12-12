6e6a112a2552f940c113dbbee1ce949a3294572d
Adnan <adbak.dev@gmail.com>
2022-12-12 10:47:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favorite movie list");
+};
+
+app.get("/", welcome);
+
+const movieHandlers = require("./movieHandlers");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+const users = require("./users");
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUsersById);
+app.post("/api/users", users.postUsers);
+app.put("/api/users/:id", users.updateUser);
+app.delete("/api/users/:id", users.deleteUser);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
